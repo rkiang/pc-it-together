78c82c56341ed955eecc1dca9982da68f9bec433
rkiang <ryanlkiang@gmail.com>
2017-09-12 14:21:05 -0500

    fixed issue, user will only GET data from their own input

--- server/routes/builds.router.js
+++ server/routes/builds.router.js
@@ -4,15 +4,23 @@ var InfoSchema = require('../models/create');
 
 router.get('/', function (req, res) {
     console.log('shelf get route was hit');
-    InfoSchema.find({}, function (err, data) {
-        if (err) {
-            console.log('find err', err);
-            res.sendStatus(500);
-        } else {
-            console.log('found data', data)
-            res.send(data)
-        }
-    })
+    if (req.isAuthenticated()) {
+        var userInfo = {
+            username: req.user.username
+        };
+        InfoSchema.find(userInfo, function (err, data) {
+            if (err) {
+                console.log('find err', err);
+                res.sendStatus(500);
+            } else {
+                console.log('found data', data)
+
+                res.send(data)
+            }
+        })
+    } else {
+        res.send(false);
+    }
 })
 
 module.exports = router;
